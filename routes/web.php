90cfe21c1e4af3baa20e21e7189f7584581d31dc
unknown <ostinsamuel25@gmail.com>
2025-08-05 16:05:11 -0500

    arreglo de nav

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
 Route::get('/nosotros', function () {
     return view('sobre_nosotros');
 });
